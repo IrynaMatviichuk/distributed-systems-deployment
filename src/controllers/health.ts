e03ab7864148eff68ddf2909d4121397eb78e330
Iryna Matviichuk <iamatviichuk@gmail.com>
2022-12-21 21:40:59 +0200

    Fix health endpoint message

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -2,5 +2,5 @@ import { jsonResponse } from '../utils/response';
 import { Request, Response } from 'express';
 
 export const health = async (req: Request, res: Response) => {
-  return jsonResponse(res, 200, 'Healthy (test CAAS)');
+  return jsonResponse(res, 200, 'Healthy');
 };
