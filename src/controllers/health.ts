f81b013beac6b6938fe5fce03f64882e6481f00d
Iryna Matviichuk <31668203+IrynaMatviichuk@users.noreply.github.com>
2022-12-27 15:04:15 +0200

    Update health.ts

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -2,5 +2,5 @@ import { jsonResponse } from '../utils/response';
 import { Request, Response } from 'express';
 
 export const health = async (req: Request, res: Response) => {
-  return jsonResponse(res, 200, 'Healthy');
+  return jsonResponse(res, 200, 'Healthy (test PAAS workflow)');
 };
